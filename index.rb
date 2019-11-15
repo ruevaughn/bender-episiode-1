a15c02f4acdfa331545515c8e8dd334b3e829d06
Chase Jensen <soccerchase@gmail.comm>
2019-11-15 05:31:07 -0700

    Updates

--- index.rb
+++ index.rb
@@ -1,48 +1,45 @@
-class Map
-  attr_accessor :rows
-  attr_accessor :rows_as_string
-
-  def initialize
-    @rows = []
-    @map = ''
+module FuturamaLand
+  class Map
+    attr_accessor :rows
+    attr_accessor :rows_as_string
+  
+    def initialize
+      @rows = []
+      @map = ''
+    end
+  
+    def insert_row(row)
+      @rows << row.lines(',')
+      @map << row
+    end
+  
+    def draw_map
+      puts @map
+    end
   end
-
-  def insert_row(row)
-    @rows << row.lines(',')
-    @map << row + '\n'
+  
+  class Bender
+    attr_accessor :direction
+    
+    def initialize
+      @direction = 'SOUTH'
+    end
   end
+  
 end
-
-class Bender
-
-  attr_accessor :direction
-  attr
-  def initialize
-    @direction = 'SOUTH'
-  end
-
-end
-def get_bender_location(row, line_index)
-  if row.index('@')
-    @bender = []
-  end
-  STDERR.puts row
-end
-
+  
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
 l, c = gets.split(" ").collect {|x| x.to_i}
-l.times do |line_index|
-  STDERR.puts gets
+l.times do
   row = gets.chomp
-  @map = Map.new
+  @map = FuturamaLand::Map.new
   @map.insert_row(row)
-  # get_bender_location(row)
 end
-STDERR.puts @map.map
+@bender = FuturamaLand::Bender.new
 
 # Write an action using puts
 # To debug: STDERR.puts "Debug messages..."
 
-puts "answer"
+puts @bender.direction
