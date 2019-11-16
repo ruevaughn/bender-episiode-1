15292b772560295c755faf108264781af5c3e4f9
Chase Jensen <soccerchase@gmail.comm>
2019-11-16 13:55:10 -0700

    Start working on state machine model

--- index.rb
+++ index.rb
@@ -4,41 +4,70 @@ module FuturamaLand
   module NewBenderFirmware
     module CompassLogic
       COMPASS_ADVICE = {
-        north: [ :column_index, :up],
-        east: [ :row_index, :up ],
-        south: [ :row_index, :down ],
-        west: [ :column_index, :down ]
+        north: [:column_index, :up],
+        east: [:row_index, :up],
+        south: [:row_index, :down],
+        west: [:column_index, :down]
       }.freeze
 
       CARDINAL_DIRECTIONS = %w[NORTH EAST SOUTH WEST].freeze
+      CARDINAL_ABBR = %w[N E S W].freeze
       NORTH = CARDINAL_DIRECTIONS[0].freeze
       EAST = CARDINAL_DIRECTIONS[1].freeze
       SOUTH = CARDINAL_DIRECTIONS[2].freeze
       WEST = CARDINAL_DIRECTIONS[3].freeze
 
-      DIRECTION_CHANGES = { 'N' => NORTH, 'E' => EAST, 'S' => SOUTH, 'W' => 'WEST' }.freeze
+      DIRECTION_CHANGES = { 'N' => NORTH, 'E' => EAST, 'S' => SOUTH, 'W' => WEST }.freeze
 
       STANDARD_DIRECTIONS = { south: SOUTH, east: EAST, north: NORTH, west: WEST }.freeze
       INVERTED_DIRECTIONS = { west: WEST, north: NORTH, east: EAST, south: SOUTH }.freeze
     end
 
+    # These firmware updates are aware of the original 'Bender' prototype...
     module BenderProgrammableLogicFirmware
       def predict_move(object)
-        predict_bender_move if can_move?(object)
+        
+          predict_bender_move(object) 
+          predict_bender_interaction(object)
+        end
       end
 
-      def can_move?(object)
-        if object == FuturamaLand::CityMap::OPEN_SPACE
-        elsif object
+
+      def predict_bender_move(object)
+        moved = false
+        if CARDINAL_ABBR.include?(object)
+          DIRECTION_CHANGES.each do |abbr, direction|
+            if object == abbr
+              moved = true
+              @direction = direction 
+            end
+          end
+        elsif @inverted
+          INVERTED_DIRECTIONS.each do
         end
+
+        predict_move_direction(object)
+        @found_booth = true if object == SUICIDE_BOOTH
+      end
+
+
+
+      def predict_move_direction(object)
+
+        direction = @direction
+        direction ||= 
       end
     end
   end
+
+  # All the firmware compiled
   module Firmware
     include FuturamaLand::NewBenderFirmware::CompassLogic
     include FuturamaLand::NewBenderFirmware::BenderProgrammableLogicFirmware
   end
 
+  # Bender - Beer, Women, and Fry are the only things on his hard drive. 
+  #          Little does he know he's about to get a firmware update
   class Bender
     attr_accessor :location
     attr_accessor :found_booth
@@ -56,10 +85,6 @@ module FuturamaLand
       @breaker_mode = false
     end
 
-    def implant_map(map)
-      @map = map
-    end
-
     def get_new_location(direction)
       row_or_column, new_direction = COMPASS_ADVICE[direction]
       if new_direction == :up
@@ -72,64 +97,105 @@ module FuturamaLand
       new_location
     end
 
+    def can_move?(object)
+      if object == OPEN_SPACE
+        true
+      elsif CARDINAL_ABBR.include?(object)
+        true
+      elsif object == SUICIDE_BOOTH
+      else
+        false
+      end
+    end
+
+    def can_interact?(object)
+      if @breaker_mode && (object == BREAKABLE_OBJECT)
+        true
+      elsif object == TELEPORTER
+        true
+      elsif object == BEER
+        true
+      elsif object == INVERTER
+      true
+      else
+      false
+      end
+    end
+
     def look_around
-      directions = inverted ? INVERTED_DIRECTIONS : STANDARD_DIRECTIONS
-      directions.each do |direction_sym, _ |
+      if @direction
+      unless @direction 
+        predict_move(object) if can_move?(object)
+        directions ||= inverted ? INVERTED_DIRECTIONS : STANDARD_DIRECTIONS
+        directions.each do |direction_sym, _|
         location_to_check = get_new_location(direction_sym)
         object = @map.object_at_location(location_to_check)
-        predict_move(object)
-        # For each direction check if can move
-        # For each direction check if can interact
-        # For each direction check if teleporter
-        # For each direction check if beer
+          # For each direction check if can move
+          # For each direction check if can interact
+          # For each direction check if teleporter
+          # For each direction check if beer
+        end
       end
     end
   end
 
-  class CityMap
-    attr_accessor :rows
-    attr_accessor :bender
-
-    OBJECTS = { '$' => :suicide_booth, 'B' => :beer, 'I' => :inverter, 'T' => :teleporters }
-    OBSTACLES = { '#' => :unbreakable, 'X' => :breakable }
-    OPEN_SPACE = ' '
-
-    def initialize(attrs={})
-      @rows = []
-      @bender = attrs[:bender]
+  module City
+    module Decapod
+      OBJECTS = { '$' => :suicide_booth, 'B' => :beer, 'I' => :inverter, 'T' => :teleporters }.freeze
+      OBSTACLES = { '#' => :unbreakable, 'X' => :breakable }.freeze
+      OPEN_SPACE = ' '
+      BREAKABLE_OBJECT = OBSTACLES['X']
+      UNBREAKABLE_OBJECT = OBSTACLES['#']
+      SUICIDE_BOOTH = OBJECTS['$']
+      BEER = OBJECTS['B']
+      INVERTER = OBJECT['I']
+      TELEPORER = OBJECT['T']
     end
 
-    def upload_to_map(row)
-      @rows << row.split('')
-    end
+    # I'm the map i'm the map i'm the map!
+    class Map
+      attr_accessor :rows
+      attr_accessor :bender
 
-    def locate_bender
-      @rows.each_with_index do |row, row_index|
-        column_index = row.index('@')
-        if column_index
-          location = { row_index: row_index, column_index: column_index }
-          mark_bender_on_map(location)
-        end
-        break if column_index
+      include City::Decapod
+
+      def initialize(attrs = {})
+        @rows = []
+        @bender = attrs[:bender]
       end
-    end
 
-    def object_at_location(location)
-      @rows[location[:row_index]][location[:column_index]]
-    end
+      def upload_to_map(row)
+        @rows << row.split('')
+      end
 
-    private
+      def locate_bender
+        @rows.each_with_index do |row, row_index|
+          column_index = row.index('@')
+          if column_index
+            location = { row_index: row_index, column_index: column_index }
+            mark_bender_on_map(location)
+          end
+          break if column_index
+        end
+      end
 
-    def mark_bender_on_map(location)
-      @bender.location = location
-    end
+      def object_at_location(location)
+        @rows[location[:row_index]][location[:column_index]]
+      end
 
-    def check_column(location)
-      @rows[location[:row_index]][location[:column_index]]
-    end
+      private
 
-    def update_map(location, symbol)
-      @map[location[:row_index]][location[:column_index]] = symbol
+      def mark_bender_on_map(location)
+        @bender.location = location
+      end
+
+      def check_column(location)
+        @rows[location[:row_index]][location[:column_index]]
+      end
+
+      def update_map(location, symbol)
+        @map[location[:row_index]][location[:column_index]] = symbol
+      end
     end
   end
 end
@@ -139,8 +205,8 @@ end
 
 l, c = gets.split(" ").collect { |x| x.to_i }
 @bender = FuturamaLand::Bender.new
-@map = FuturamaLand::CityMap.new(bender: @bender)
-@bender.implant_map(@map)
+@map = FuturamaLand::City::Map.new(bender: @bender)
+@bender.map = @map
 l.times do
   row = gets.chomp
   @map.upload_to_map(row)
